9e6a21cabde09e599fb990cabb0998d2114554d1
Platy-2744 <jitisakbigilj132109@gmail.com>
2025-02-05 18:23:02 +0700

    Incomplete001

--- app/src/main/java/com/example/wisage/MainActivity.kt
+++ app/src/main/java/com/example/wisage/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.wisage
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var tolog: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        init()
+        tolog!!.setOnClickListener{
+            startActivity(Intent(this,Login::class.java))
+        }
+    }
+    private fun init(){
+        tolog = findViewById(R.id.main_btn_tologin)
     }
 }
